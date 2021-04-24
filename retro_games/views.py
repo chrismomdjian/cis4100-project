806b8172883c1fc6916c3d0881ab668668d89baf
Christian Momdjian <chrismomdjian@gmail.com>
2021-04-24 16:07:01 -0700

    added more information modal

--- retro_games/views.py
+++ retro_games/views.py
@@ -13,27 +13,42 @@ def results(request):
 	if request.method == 'POST':
 		search_title = request.POST.get('search_title')
 		games_query = api_call('games', 
-			'fields age_ratings,aggregated_rating,category,cover,first_release_date,' + 
+			'fields id,age_ratings,aggregated_rating,category,cover,first_release_date,' + 
 			'genres,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,' +
 			'rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,status,' +
 			'storyline,summary,tags,url;' +
 			'search "' + search_title + '";')
 
 		for game in games_query:
+			screenshot_urls = []
+			screenshot_ids = []
+
 			try:
 				summary = game['summary']
 			except KeyError:
 				summary = 'N/A'
+
+			try:
+				storyline = game['storyline']
+			except:
+				storyline = 'N/A'
+
+			try:
+				release_date = datetime.utcfromtimestamp( game['first_release_date'] ).strftime('%m-%d-%Y')
+			except:
+				release_date = 'N/A'
 
 			cover_query = api_call('covers',
 				'fields alpha_channel,animated,checksum,game,height,image_id,url,width;' +
 				'where game=' + str(game['id']) + ';')
 
 			all_games.append({
+				'id': game['id'],
 				'title': game['name'],
-				'release_date': datetime.utcfromtimestamp( game['first_release_date'] ).strftime('%m-%d-%Y'),
+				'release_date': release_date,
 				'cover': 'https:' + cover_query[0]['url'],
 				'summary': summary,
+				'storyline': storyline,
 			})
 
 	return render(request, 'results.html', {'all_games': all_games})
